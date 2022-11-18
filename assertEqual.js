9475f897768c3fd014910b03d3211a62e0ff182e
eli <eliitheboss@gmail.com>
2022-11-18 07:02:11 +0000

    updated with template literal strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion passed: 🥳️🥳️ [actual] === [expected]");
+    console.log(`Assertion passed: 🥳️🥳️ ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed: 🤧🤧 [actual] !== [expected]");
+    console.log(`Assertion Failed: 🤧🤧 ${actual} !== ${expected}`);
   }
   
 };
+
 console.log(assertEqual(1, 2));
 console.log(assertEqual("yes", "maybe"));
 console.log(assertEqual("yes", "yes"));
