2dc44e4093704b42ab0c802157108c10e7fcbe3a
eli <eliitheboss@gmail.com>
2022-11-21 01:49:12 +0000

    function to remove items from an array

---
+++ without.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let filteredArray = [];
+  for (const element of source) {
+    if (!itemsToRemove.includes(element)) {
+      filteredArray.push(element);
+    }
+  }
+  return filteredArray;
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+let source = ["a", "b", "s", "g", "z"];
+assertArraysEqual((source, ["a", "s"]))
+
+
+
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
