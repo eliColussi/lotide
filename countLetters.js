ed9964364f5a40fcf40b1f2eb815bf0c8d269bbf
eli <eliitheboss@gmail.com>
2022-11-23 06:38:33 +0000

    returns count of number of letters in string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: 🥳️🥳️ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: 🤧🤧 ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+      
+    }
+  }
+    return result;
+};
+
+
+
+
+
+
+console.log(countLetters("aaeoiuutbhgdts"));
