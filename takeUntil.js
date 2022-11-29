b04ff96f61adbd4cc20905c4597006cd1489230b
eli <eliitheboss@gmail.com>
2022-11-29 02:46:47 +0000

    function that takes a slice of the array with elements taken from the beginning

---
+++ takeUntil.js
@@ -0,0 +1,36 @@
+const { default: test } = require("node:test");
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+//implementing our takeUntil function
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback === array[item]) {
+      results = array.slice(0, item);
+    }
+  }
+  return results;
+};
+
+const arr1 = ["4", "7", "3", "100", "30"];
+const result1 = takeUntil(arr1, x => x === 100);
+console.log(result1)
