003af453330b74ff15c716914fc63608a3b7b9ed
eli <eliitheboss@gmail.com>
2022-12-08 02:31:41 +0000

    tests for tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,4 @@
 const tail = require("../tail");
 
 const words = ["YoYo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
+console.log(tail(words)); 
