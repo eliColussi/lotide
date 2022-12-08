3e47993a081391117449f4fa02af330a741e0907
eli <eliitheboss@gmail.com>
2022-12-08 03:12:13 +0000

    tests for middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,5 @@
 const middle = require("../middle");
+const assertArraysEqual = require("../assertArraysEqual");
 
-console.log(middle([1, 3, 6, 4, 3, 7, 4, 8]));
+
+console.log(middle(assertArraysEqual([1, 3, 6, 4, 3, 7, 4, 8])));
